940837f67320bf9b8b685875089b4393be33c891
Josey Morton <me@joseymorton.com>
2012-06-21 14:57:59 -0500

    adding support for multilevel 'child' and 'parent' requests

--- extensions/inheritance/Parser.php
+++ extensions/inheritance/Parser.php
@@ -31,10 +31,10 @@ class Parser {
 	protected static $_terminals;
 
 	/**
-	 * Template source code
-	 * @var blob
+	 * array of template files
+	 * @var array
 	 */
-	protected static $_source;
+	protected static $_templates;
 
 	/**
 	 * `BlockSet` object, set from render method
@@ -48,18 +48,28 @@ class Parser {
 	 * @param  object 	$blocks `BlockSet` object
 	 * @return [type]         [description]
 	 */
-	public static function run($source, $blocks){
+	public static function parse($blocks){
 
 		static::$_terminals = array_flip(Lexer::terminals());
-
-		static::$_source = $source;
 
 		static::$_blocks = $blocks;
 
 		$_pattern = "/{:(block) \"({:block})\"(?: \[(.+)\])?}(.*){\\1:}/msU";
-		
-		return static::_replace(static::$_source, $_pattern);
 
+		static::$_templates = array_reverse($blocks->templates());
+		
+		$i = 0;
+		while ($i < $count = count(static::$_templates)) {
+
+			if($i == $count) break;
+			$source = static::_read(static::$_templates[$i]);
+			static::_replace($source);
+			$i++;
+
+		}
+
+		// final parent template handler
+		return static::$_templates[0];
 	}
 
 	/**
@@ -67,45 +77,139 @@ class Parser {
 	 * @param  blob $source template source
 	 * @return blob         template source, replaced with blocks
 	 */
-	private static function _replace($source = null, $pattern){
+	private static function _replace($source, $type = 'prep'){
 
-		if($source == null) $source = static::$_source;
+		$pattern = "/{:(block) \"({:block})\"(?: \[(.+)\])?}(.*){\\1:}/msU";
 
 		preg_match_all(static::$_terminals['T_BLOCK'], $source, $matches);
 
 		foreach($matches[2] as $index => $block){
 
+
 			$_pattern = String::insert($pattern, array('block' => $block));
 
 			$_block = static::$_blocks->blocks("{$block}");
 
-			$content = $_block->content();
+			/**
+			 * Top level content for block
+			 * @var string
+			 */
+			$content = trim($_block->content());
 
 			/**
-			 * Child matches/replacement
+			 * The request for block content in the final template
+			 * @var string
 			 */
-			if(preg_match("/{:child:}/msU", $matches[4][$index])){
+			$request = $matches[4][$index];
 
-				if(!$_block->parent()) {
-					$content = preg_replace($_pattern, "", $matches[0][$index]);
-				} else {
-					$content = preg_replace("/{:child:}/msU", $content, $matches[4][$index]);
+			/**
+			 * Parent/child matches/replacement
+			 */
+			$_parents = function($block) use (&$content, &$_parents, &$matches, &$index, &$_pattern){
+
+				$parent = $block->parent();
+
+				if(preg_match("/{:([a-zA-Z]+):}/msU", $content, $_matches)) { 
+
+					if($_matches[1] == 'parent') {
+
+							$content = preg_replace("/{:$_matches[1]:}/msU", $parent->content(), $content);
+
+							// go again
+							return $_parents($block->parent(), $content);
+							break;
+
+					}
+
 				}
 
-			}
+				return $content;
 
-			/**
-			 * Parent matches/replacement
-			 */
-			if(preg_match("/{:parent:}/msU", $content)) { 
-				$content = preg_replace("/{:parent:}/msU", $_block->parent()->content(), $content);
-			}
+			};
+
+			$_blocks = static::$_blocks;
+			$_children = function($block) use (&$content, &$_children, &$matches, &$index, &$_pattern, &$_blocks){
+
+				$blockContent = $block->content();
+				$_block = $_blocks->blocks($block->name());
+
+				/**
+				 * If the block has a child then we're not at the bottom of the chain.
+				 * We need to move up until we cant
+				 * @var mixed `object` or `false`
+				 */
+				$child = $block->child();
+
+				/**
+				 * If the block has a parent then we cant be at the top of the chain.
+				 * As long as there's a parent we need to keep moving. 
+				 * @var mixed `object` or `false`
+				 */
+				$parent = $block->parent();
+
+				if(preg_match("/{:child:}/msU", $blockContent)) { 
+
+					// Block doesn't have a child block
+					if(!$child){
+						// Also has no parent
+						if(!$parent){
+							// clear the entire block
+							$content = "";
+						} else {
+							// Has a parent, still no child tho
+							// just remove the call for child block
+							$content = preg_replace("/{:child:}/msU", "", $blockContent);
+						}
+					} else {
+						// replace with child contents
+						// return $_children($child);
+						// print_r($block->name());
+						$content = preg_replace("/{:child:}/msU", $_block->content(), $blockContent);
+					}
+
+				// not asking for a child
+				} else {
+
+					if($parent){
+						return $_children($parent);
+					}
+
+				}
+
+				// must return content so we dont muck up parent
+				return $content;
+
+			};
+
+			$content = $_children($_block);
+			$content = $_parents($_block);
+
+			// assign the parsed content to the block
+			// $_block->parsed($content);
 
 			$source = preg_replace($_pattern, $content, $source);
 
 		}
 
 		return $source;
+
+	}
+
+	private static function _read($template){
+
+		if(file_exists($template)){
+
+			$content = file_get_contents($template);
+
+			return $content;
+
+		}
+
+	}
+
+	public static function finish($source){
+
+		return static::_replace($source);
 
 	}
 
